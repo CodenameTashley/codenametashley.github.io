310333af65bbb2ffec2ae757ffd0c6ef8abcfb92
Tashley <tashleyvivesh@gmail.com>
2017-09-25 21:41:19 +0400

    #14 Added bower components for angular fire & firebase. Retrieve from firebase and updated about me

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var mainApp = angular.module("TashleyApp", ['ui.router']);
+var mainApp = angular.module("TashleyApp", ['ui.router', 'firebase']);
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCUk7AcNP8SecGxE-kugJpKN5VvIwMJjqE",
+    authDomain: "codenametashley-a2b9a.firebaseapp.com",
+    databaseURL: "https://codenametashley-a2b9a.firebaseio.com",
+    projectId: "codenametashley-a2b9a",
+    storageBucket: "codenametashley-a2b9a.appspot.com",
+    messagingSenderId: "762388539776"
+  };
+  firebase.initializeApp(config);
--- js/main.controller.js
+++ js/main.controller.js
@@ -1,4 +1,7 @@
-angular.module("TashleyApp").controller('mainController', function($location, $window){
+angular.module("TashleyApp").controller('mainController', function($location, $window, $firebaseObject){
     var vm = this;
+    var ref = firebase.database().ref();
+    this.nameTest = $firebaseObject(ref);
+    //console.log(this.nameTest);
     vm.year = new Date().getFullYear();
 });
