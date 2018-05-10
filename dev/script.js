68ffaa367899ef3f997ac74087423e337f1fdba6
Tashley <tashley.toocaram@chb-technologies.ch>
2018-05-10 14:07:56 +0400

    #22 V2.0
    Birthday Release

---
+++ dev/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#profile-card").click(function(){
+        $("#profile-info").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#resume-card").click(function(){
+        $("#resume-info").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#reference-card").click(function(){
+        $("#reference-info").slideToggle("slow");
+    });
+});
---
+++ script.js
@@ -0,0 +1,30 @@
+/* Session Stack code */ ! function (a, b) {
+    var c = window;
+    c.SessionStack = a, c[a] = c[a] || function () {
+        c[a].q = c[a].q || [], c[a].q.push(arguments)
+    }, c[a].t = b;
+    var d = document.createElement("script");
+    d.async = 1, d.src = "https://cdn.sessionstack.com/sessionstack.js";
+    var e = document.getElementsByTagName("script")[0];
+    e.parentNode.insertBefore(d, e);
+}("sessionstack", "296508c878fa469cb8993f85680d9379");
+
+/* End of Session Stack code */
+
+$(document).ready(function () {
+    $("#profile-card").click(function () {
+        $("#profile-info").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#resume-card").click(function () {
+        $("#resume-info").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#reference-card").click(function () {
+        $("#reference-info").slideToggle("slow");
+    });
+});
