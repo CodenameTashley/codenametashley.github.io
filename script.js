933bf8265526eac50af5fff334c4ed7f05cfc949
Tashley <tashley.toocaram@elca.mu>
2020-05-02 12:25:21 +0400

    add new website update

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/* Session Stack code */ 
-! function (a, b) {
-    var c = window;
-    c.SessionStack = a, c[a] = c[a] || function () {
-        c[a].q = c[a].q || [], c[a].q.push(arguments)
-    }, c[a].t = b;
-    var d = document.createElement("script");
-    d.async = 1, d.src = "https://cdn.sessionstack.com/sessionstack.js";
-    var e = document.getElementsByTagName("script")[0];
-    e.parentNode.insertBefore(d, e);
-}("sessionstack", "296508c878fa469cb8993f85680d9379");
-
-/* End of Session Stack code */
-
 /* $(document).ready(function () {
     $("#profile-card").click(function () {
         $("#profile-info").slideToggle("slow");
